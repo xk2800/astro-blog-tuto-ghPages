695a2cf0f942cdca730e1aa715b0094f1d5129fe
Xavier K. <59243836+xk2800@users.noreply.github.com>
2024-02-09 00:35:03 +0800

    test astro config base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-blog-tuto-ghPages.xk2800.github.io',
+  base: '/astro-blog-tuto-ghPages',
   integrations: [preact()]
 });
